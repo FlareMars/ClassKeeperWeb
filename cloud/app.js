2f29d89b96269d6f49170a17a7ef19b359f16fc2
FlareMars <a773207723@qq.com>
2015-05-27 02:46:30 +0800

    推送测试

--- cloud/app.js
+++ cloud/app.js
@@ -29,20 +29,20 @@ app.post('/upload', function(req, res) {
             });
             theFile.save().then(function(theFile) {
 
-                return res.send('上传成功！文件ID為 : ');
+                return res.send('上传成功！文件ID為 : ' + theFile.id);
                 //推送數據到指定用戶
-                // var query = new AV.Query("_Installation");
-                // query.equalTo("userId", targetUserId);
-                // AV.Push.send({
-                //     appId: "q77fhkht4neg4ixnybwjnjmodatcoxy4wplq6ocb9lrzy5hs",
-                //     appKey: "vhvdk35bg5p6zsxdsp5boqz2hckljc2djbc7c12834bdj5mv",
-                //     where: query,
-                //     data: {
-                //         action: "cm.action.MESSAGE",
-                //         msg_type: 12,
-                //         content_target_id: theFile.get("objectId")
-                //     }
-                // });
+                var query = new AV.Query("_Installation");
+                query.equalTo("userId", targetUserId);
+                AV.Push.send({
+                    appId: "q77fhkht4neg4ixnybwjnjmodatcoxy4wplq6ocb9lrzy5hs",
+                    appKey: "vhvdk35bg5p6zsxdsp5boqz2hckljc2djbc7c12834bdj5mv",
+                    where: query,
+                    data: {
+                        action: "cm.action.MESSAGE",
+                        msg_type: 12,
+                        content_target_id: theFile.id
+                    }
+                });
 
                 // res.send('上传成功！文件ID為 : ' + theFile.get("objectId"));
             });
