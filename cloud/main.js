87b9ba4d0eb0d03fc204a8fd91b405b264e3754c
FlareMars <a773207723@qq.com>
2015-05-27 00:39:58 +0800

    新增getClassCode云端函数

--- cloud/main.js
+++ cloud/main.js
@@ -43,7 +43,7 @@ query.find({
                 temp = '0' + temp;
             }
             var subtemp = temp.substr(0, 5);
-            var result = prefix + subtemp;
+            var result = prefix + subtemp + "xx";
             return result;
         }
         
