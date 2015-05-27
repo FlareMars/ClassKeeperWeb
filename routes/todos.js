7413de208bba2ac89901d6fcc812b0293c42034a
FlareMars <a773207723@qq.com>
2015-05-28 00:35:26 +0800

    新增登陆功能

--- routes/todos.js
+++ routes/todos.js
@@ -43,6 +43,23 @@ router.post('/', function(req, res, next) {
       next(err);
     }
   })
-})
+});
+
+//登陆
+router.post('/login',function(req,res,next) {
+  AV.User.logIn(req.body.username, req.body.password).then(function() {
+        //登录成功，avosExpressCookieSession会自动将登录用户信息存储到cookie
+        // res.redirect('/UploadFile');
+        if (req.AV.user) {
+            // 如果已经登录，发送当前登录用户信息。
+            res.send(req.AV.user);
+        } else {
+            res.send('登陸失敗!')
+        }
+
+    }, function(error) {
+        res.send('登陸失敗!' + error.message + ' ' + error.code);
+    });
+});
 
 module.exports = router;
